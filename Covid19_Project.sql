f3f8c916e7b4fa7ebae42d2a25eedcf51bf85dc1
Mohamed Hassan <mohamedhassan006899@gmail.com>
2023-12-22 03:44:10 +0200

    Add files via upload

--- Covid19_Project.sql
+++ Covid19_Project.sql
@@ -1,78 +1,105 @@
 select *
-from PortfolioProject..CovedDeath 
+	from PortfolioProject..CovedDeath 
 order by 3,4
 
 select *
-from PortfolioProject..CovedVaccinations
+	from PortfolioProject..CovedVaccinations
 order by 3,4
 
-SELECT location , date , total_cases , total_deaths , new_cases , population 
-FROM PortfolioProject..CovedDeath
+SELECT 
+	location ,
+	date , 
+	total_cases ,
+	total_deaths ,
+	new_cases ,
+	population 
+		FROM PortfolioProject..CovedDeath
 order by 1 , 2;
 
 
-SELECT location , date , total_cases , total_deaths , total_deaths / NULLIF(total_cases, 0) * 100 AS Deathpercentage
-FROM PortfolioProject..CovedDeath
-where location  like 'eg%' 
-order by 1 , 2;
+SELECT 
+	location , 
+	date , 
+	total_cases , 
+	total_deaths , 
+	total_deaths / NULLIF(total_cases, 0) * 100 AS Deathpercentage
+		FROM PortfolioProject..CovedDeath
+	where location  like 'eg%' 
+	order by 1 , 2;
 
 -- Looking at Total Cases vs Population
 
-SELECT location , date , population , total_cases  , total_cases / NULLIF(population, 0) * 100 AS PercentPopulationInfected
-FROM PortfolioProject..CovedDeath
-where location  like 'eg%' 
-order by 1 , 2;
+SELECT 
+	location , 
+	date , 
+	population , 
+	total_cases  , 
+	total_cases / NULLIF(population, 0) * 100 AS PercentPopulationInfected
+		FROM PortfolioProject..CovedDeath
+	where location  like 'eg%' 
+	order by 1 , 2;
 
 
 
 -- Looking at countries with highest infection rates Compared to Population
 
-SELECT location , population , max(total_cases) as HighestInfectionCount  ,
-max(total_cases / NULLIF(population, 0)) * 100 AS PercentPopulationInfected
-FROM PortfolioProject..CovedDeath
+SELECT 
+	location , 
+	population , 
+	max(total_cases) as HighestInfectionCount  ,
+	max(total_cases / NULLIF(population, 0)) * 100 AS PercentPopulationInfected
+		FROM PortfolioProject..CovedDeath
 --where location  like 'eg%' 
-Group by location , population
-order by PercentPopulationInfected desc;
+	Group by location , population
+	order by PercentPopulationInfected desc;
 
 
 
 -- Showing Countries With Highest Death Count Per Population 
 
-SELECT location  , max(total_deaths) as TotalDeathCount  
-FROM PortfolioProject..CovedDeath 
+SELECT 
+	location , 
+	max(total_deaths) as TotalDeathCount  
+		FROM PortfolioProject..CovedDeath 
 --where location  like 'eg%' 
-Group by location
-order by TotalDeathCount desc;
+	Group by location
+	order by TotalDeathCount desc;
 
 
 -- Showing Continent  With Highest Death Count Per Population 
 
-SELECT Continent  , max(total_deaths) as TotalDeathCount  
-FROM PortfolioProject..CovedDeath 
+SELECT 
+	Continent  , 
+	max(total_deaths) as TotalDeathCount  
+		FROM PortfolioProject..CovedDeath 
 --where location  like 'eg%'
-where Continent != ' '
-Group by Continent
-order by TotalDeathCount desc;
+	where Continent != ' '
+	Group by Continent
+	order by TotalDeathCount desc;
 
 
 -- GLOBAL NUMBERS
 
-SELECT date , sum(total_cases) as Total_Cases ,sum(new_deaths) as Total_Deaths ,
-(sum(new_deaths) / nullif(sum(new_cases) ,0)) *100 as Death_Percentage 
-FROM PortfolioProject..CovedDeath
+SELECT date , 
+	sum(total_cases) as Total_Cases ,
+	sum(new_deaths) as Total_Deaths ,
+	(sum(new_deaths) / nullif(sum(new_cases) ,0)) *100 as Death_Percentage 
+		FROM PortfolioProject..CovedDeath
 --where location  like 'eg%' 
-where Continent IS NOT NULL
-group by date
-order by 1 , 2;
+	where Continent IS NOT NULL
+	group by date
+	order by 1 , 2;
 
 -- Total_Cases , Total_Deaths And The Death Percentage 
 
-SELECT sum(total_cases) as Total_Cases ,sum(new_deaths) as Total_Deaths ,
-(sum(new_deaths) / nullif(sum(new_cases) ,0)) *100 as Death_Percentage 
-FROM PortfolioProject..CovedDeath
+SELECT 
+	sum(total_cases) as Total_Cases ,
+	sum(new_deaths) as Total_Deaths ,
+	(sum(new_deaths) / nullif(sum(new_cases) ,0)) *100 as Death_Percentage 
+		FROM PortfolioProject..CovedDeath
 --where location  like 'eg%' 
-where Continent != ' '
-order by 1 , 2;
+	where Continent != ' '
+	order by 1 , 2;
 
 -- Calculation total population vs vaccinations
 
@@ -94,12 +121,12 @@ order by 1 ,2
 drop table if exists #percentPopulationVaccinated
 create table #percentPopulationVaccinated
 (
-Continent nvarchar(255),
-Location nvarchar (255),
-Date datetime ,
-Population numeric ,
-New_vaccinations numeric,
-Rollingpeoplevaccinated numeric
+	Continent nvarchar(255),
+	Location nvarchar (255),
+	Date datetime ,
+	Population numeric ,
+	New_vaccinations numeric,
+	Rollingpeoplevaccinated numeric
 )
 insert into #percentPopulationVaccinated
 select 
